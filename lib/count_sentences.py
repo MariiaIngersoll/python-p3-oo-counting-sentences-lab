aecc5f7d697a3d05ed7f701ca165f2c7edf7c725
+ <+>
2023-06-04 21:38:21 -0400

    completed

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,39 @@
-#!/usr/bin/env python3
-
+import re
 class MyString:
-  pass
+    def __init__(self, value = ""):
+        self._value = value
+
+    def value(self):
+        return self._value
+
+    def set_value(self, new_value):
+        if isinstance(new_value, str):
+            self._value = new_value
+        else:
+            print("The value must be a string.")
+
+    value = property(value, set_value)
+
+    def is_sentence(self):
+        if self._value.__contains__('.'):
+            return True
+        else:
+            return False
+      
+    def is_question(self):
+        if self._value.__contains__('?'):
+            return True
+        else:
+            return False
+        
+    def is_exclamation(self):
+        if self._value.__contains__('!'):
+            return True
+        else:
+            return False
+        
+    def count_sentences(self):
+        if self.value == '':
+            return 0
+            
+        return len(re.split(r'[!\?\.](?= )', self.value))
